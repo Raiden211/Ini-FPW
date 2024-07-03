4309470cd863de7e26e62c3a4d7ef1cd6fd2c10f
BijiNaga <72716980+Raiden211@users.noreply.github.com>
2024-07-03 23:39:01 +0800

    Update index.js

--- cobakoneksi/backend/index.js
+++ cobakoneksi/backend/index.js
@@ -5,11 +5,13 @@ const func = require("./function");
 const db = require("./config");
 const port = 3000;
 const cors = require('cors');
+
 app.use(cors());
+app.use(express.json());
 
 app.post("/games/post",func.cobaAdd);
-app.put("/games/edit",func.cobaEdit);
-app.delete("/games/delete",func.cobaDelete);
+app.put("/games/:id",func.cobaEdit);
+app.delete("/games/:id",func.cobaDelete);
 app.get("/games/get",func.getAllGames);
 app.get("/games/get1/:id",func.getOneGames);
 
