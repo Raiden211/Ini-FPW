768cbe36acb110c415e39c8143bd659d9151b931
BijiNaga <72716980+Raiden211@users.noreply.github.com>
2024-07-03 23:39:47 +0800

    Update DetailData.jsx
    Update edit delete, sama perbaikan sedikit

--- cobakoneksi/frontend/src/DetailData.jsx
+++ cobakoneksi/frontend/src/DetailData.jsx
@@ -1,10 +1,30 @@
 import { useState, useEffect } from 'react';
 import client from './connection';
 import { object } from 'joi';
+import { useForm } from "react-hook-form";
+import Joi from "joi";
+import { joiResolver } from "@hookform/resolvers/joi";
 
 const DetailData = (props) => {
     const [gamedata, setGamedata] = useState(null);
     const [editing, setEditing] = useState(false);
+
+    const schema = Joi.object({
+        title: Joi.string().required().messages({
+            "string.empty": "Title gaboleh kosong"
+        }),
+        image: Joi.string().required().messages({
+            "string.empty": "Url image gaboleh kosong"
+        }),
+        price: Joi.number().min(1000).required().messages({
+            "number.empty": "Harga gaboleh kosong",
+            "number.min": "Harga harus lebih dari 1000"
+        }),
+    });
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
+       resolver: joiResolver(schema)
+    });
 
     const getData = async () => {
         try {
@@ -17,25 +37,37 @@ const DetailData = (props) => {
         }
     };
 
-    const editData = async () => {
+    const editData = async (data) => {
         try {
-
+            const response = await client.put(`games/${props.index}`, data);
+            alert("Game ini telah diedit");
+            setEditing(false);
+            getData();
         } catch (error) {
-
+            console.error('Error fetching data:', error);
+            alert("Terjadi kesalahan. Silakan coba lagi.");
         }
     }
 
-    const deleteData = async () => {
-        try {
+    const onSubmit = (data) => {
+        editData(data);
+    };
 
+    const deleteData = async (id) => {
+        try {
+            const response = await client.delete(`games/${id}`);
+            alert("Game ini telah dihapus");
+            props.setRoute("index");
         } catch (error) {
-            
+            console.error('Ada masalah:', error);
+            alert("Terjadi kesalahan. Silakan coba lagi.");
         }
     }
 
     useEffect(() => {
         getData();
     }, [props.index]);
+
 
     if (!gamedata) {
         return <div>Loading...</div>;
@@ -65,11 +97,45 @@ const DetailData = (props) => {
                 <h3>Liat data</h3>
                 <button className='btn btn-primary' onClick={() => props.setRoute("index")}>Back</button>
             </div>
+            {editing ? 
+            <div key={gamedata.id} style={containerStyle}>
+                    <div className="container py-5 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-md-8 col-lg-6 col-xl-5">
+                        <div className="card shadow-2-strong" style={{borderRadius: "1rem"}}>
+                        <div className="card-body p-5 text-center">
+                            <h1>Edit games</h1>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <p>Title</p>
+                                <input type="text" {...register("title")} defaultValue={gamedata.title} className="form-control" />
+                                {errors.title && <span style={{ color: "red" }}>{errors.title.message} <br /></span>}
+                                <p>Image url</p>
+                                <input type="text" {...register("image")} defaultValue={gamedata.image} className="form-control" />
+                                {errors.image && <span style={{ color: "red" }}>{errors.image.message} <br /></span>}
+                                <p>Price</p>
+                                <input type="number" {...register("price")} defaultValue={gamedata.price} className="form-control" />
+                                {errors.price && <span style={{ color: "red" }}>{errors.price.message} <br /></span>}
+                                <br />
+                                <br />
+                                <button type="submit" className="btn btn-success">Edit</button>
+                                <button className='btn btn-warning' style={{marginLeft: '30px'}} onClick={() => setEditing(false)}>Back</button>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                    </div>    
+                </div>
+            </div>
+            : 
             <div key={gamedata.id} style={containerStyle}>
                 <img src={gamedata.image} alt={gamedata.title} style={imageStyle} />
-                <p>{gamedata.title}</p>
-                <p>{gamedata.price}</p>
-            </div>
+                <p>Judul : {gamedata.title}</p>
+                <p>Harga : {gamedata.price}</p>
+                <div>
+                    <button className='btn btn-warning' style={{marginRight: '30px'}} onClick={() => setEditing(true)}>Edit</button>
+                    <button className='btn btn-danger' onClick={() => deleteData(gamedata.id)}>Delete</button>
+                </div>
+            </div>}
         </div>
     );
 };
